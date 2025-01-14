228dea6e50a63167d4f2c05931ca1ed294ab73c0
SUnilRajbanshi <sunil19rajbanshi@gmail.com>
2025-01-14 08:29:33 +0545

    The file has been committed

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The Student Management System!");
+        System.out.println("The Student Management System:");
     }
 }
---
+++ Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getter methods (optional, but useful)
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setter methods (optional)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
